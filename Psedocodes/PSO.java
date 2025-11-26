cfa95d2f882a7bb76aca4eb6cbdc0ce74cd937da
Zain Mahedi <zainmahedi.cs23@bmsce.ac.in>
2025-11-27 01:13:20 +0530

    Update PSO.java

--- Psedocodes/PSO.java
+++ Psedocodes/PSO.java
@@ -10,38 +10,43 @@ ParticleSwarmOptimization():
     for each particle i in 1..num_particles:
         position[i] = random_position()
         velocity[i] = random_velocity()
-        personal_best_position[i] = position[i]
-        personal_best_fitness[i] = evaluate(position[i], objective_function)
 
-    // Determine global best
-    global_best_position = position of particle with highest fitness
-    global_best_fitness = fitness(global_best_position)
+        // pbest = personal best
+        pbest_position[i] = position[i]
+        pbest_fitness[i] = evaluate(position[i], objective_function)
+
+    // gbest = global best among all pbest
+    gbest_position = position of particle with highest pbest_fitness
+    gbest_fitness = best value among pbest_fitness
 
     iteration = 0
 
     while iteration < max_iterations and not convergence_reached():
 
-        // 4. Evaluate Fitness
         for each particle i:
+
+            // 4. Evaluate Fitness
             fitness = evaluate(position[i], objective_function)
 
-            if fitness better than personal_best_fitness[i]:
-                personal_best_position[i] = position[i]
-                personal_best_fitness[i] = fitness
+            // Update pbest
+            if fitness better than pbest_fitness[i]:
+                pbest_position[i] = position[i]
+                pbest_fitness[i] = fitness
 
-            if fitness better than global_best_fitness:
-                global_best_position = position[i]
-                global_best_fitness = fitness
+                // Update gbest if needed
+                if fitness better than gbest_fitness:
+                    gbest_position = position[i]
+                    gbest_fitness = fitness
 
         // 5. Update Velocities and Positions
         for each particle i:
             velocity[i] = w * velocity[i]
-                          + c1 * random() * (personal_best_position[i] - position[i])
-                          + c2 * random() * (global_best_position - position[i])
+                          + c1 * random() * (pbest_position[i] - position[i])
+                          + c2 * random() * (gbest_position - position[i])
 
             position[i] = position[i] + velocity[i]
 
         iteration = iteration + 1
 
     // 7. Output the Best Solution
-    return global_best_position, global_best_fitness
+    return gbest_position, gbest_fitness
